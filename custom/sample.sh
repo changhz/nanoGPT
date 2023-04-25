f282727270b3d64efec0a694e391e25db1c35c09
changhz <hz.chang@pm.me>
2023-04-25 13:43:10 -0400

    train shakespeare on baby gpt

--- custom/sample.sh
+++ custom/sample.sh
@@ -3,5 +3,6 @@ python sample.py \
   --out_dir=$1 \
   --device=cpu \
   --init_from=resume \
-  --start="What is the answer to life, the universe, and everything?" \
-  --num_samples=5 --max_new_tokens=100
+  --num_samples=1 \
+  --max_new_tokens=10 \
+  --start="$2"
--- custom/train.sh
+++ custom/train.sh
@@ -2,13 +2,13 @@
 python train.py $1 \
   --device=cpu \
   --compile=False \
-  --eval_iters=20 \
-  --log_interval=1 \
   --block_size=64 \
   --batch_size=12 \
   --n_layer=4 \
   --n_head=4 \
   --n_embd=128 \
-  --lr_decay_iters=2000 \
+  --log_interval=1 \
+  --eval_iters=20 \
+  --max_iters=100 \
   --dropout=0.0 \
-  $2
+  --init_from="$2"
