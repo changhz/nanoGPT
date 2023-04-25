e26379adad3e000f00862809753849f6baae56e0
changhz <hz.chang@pm.me>
2023-04-25 14:02:24 -0400

    train sh

--- custom/train.sh
+++ custom/train.sh
@@ -2,13 +2,14 @@
 python train.py $1 \
   --device=cpu \
   --compile=False \
+  --eval_iters=20 \
+  --log_interval=1 \
+  --eval_interval=40 \
+  --max_iters=120 \
   --block_size=64 \
   --batch_size=12 \
   --n_layer=4 \
   --n_head=4 \
   --n_embd=128 \
-  --log_interval=1 \
-  --eval_iters=20 \
-  --max_iters=100 \
   --dropout=0.0 \
   --init_from="$2"
